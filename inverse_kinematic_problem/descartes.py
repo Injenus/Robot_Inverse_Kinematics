0d7f190fb1964a0ca663aa1793beae73ffe57fa4
Injenus <injenus777@gmail.com>
2021-11-24 11:02:14 +0300

    Add exception handling

--- inverse_kinematic_problem/descartes.py
+++ inverse_kinematic_problem/descartes.py
@@ -1,5 +1,10 @@
 #   двумерный робот декарт
 #   q1=x
 #   q2=y
-x,y=map(float, input('\nВведите координаты x, y через пробел: ').split(' '))
-print('q1 = {}\nq2 = {}'.format(x, y))
+while True:
+    try:
+        x,y=map(float, input('\nВведите координаты x, y через пробел: ').split(' '))
+        print('q1 = {}\nq2 = {}'.format(x, y))
+        break
+    except ValueError:
+        print('Неверный формат данных.')
